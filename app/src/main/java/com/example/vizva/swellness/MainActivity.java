a8f183b2f43e13b4b90dfaf3f9b34f4732539a98
ishwarya-pasala <pasala_ishwarya@yahoo.com>
2018-04-09 18:26:13 -0400

    Made MainActivity(login screen) the first viewed page. Add user login info.

--- app/src/main/java/com/example/vizva/swellness/MainActivity.java
+++ app/src/main/java/com/example/vizva/swellness/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.vizva.swellness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected AutoCompleteTextView userTextView;
+    protected AutoCompleteTextView passwordTextView;
+    protected Button loginBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userTextView = (AutoCompleteTextView) findViewById(R.id.userName);
+        passwordTextView = (AutoCompleteTextView) findViewById(R.id.userPassword);
+        loginBut = (Button) findViewById(R.id.reg);
+
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validatePassword(userTextView.getText().toString(), passwordTextView.getText().toString());
+            }
+        });
+
+    }
+
+    private void validatePassword(String username, String password) {
+        if((username.equals("cmsc434")) && (password.equals("test"))) {
+            Intent intent = new Intent(MainActivity.this, MainpageActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
