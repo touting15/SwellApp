6befcf3f7b5a19adb2769d2d8cde5b0451e3c28d
touting15 <touting15@gmail.com>
2018-04-09 20:46:02 -0400

    splash screen added

---
+++ app/src/main/java/com/example/vizva/swellness/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.vizva.swellness;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        // Start home activity
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+
+                                  }, SPLASH_TIME_OUT);
+        // close splash activity
+    }
+}
